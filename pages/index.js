64c107f879d16b54530ee0832b5ffa6ae3b1f5e4
Amel <amcenp@gmail.com>
2020-03-11 01:14:32 +0100

    Pagination Added

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,18 @@
 import fetch from 'isomorphic-fetch';
 import Error from 'next/error';
+import Link from 'next/link';
 
 import StoryList from '../components/StoryList';
 import Layout from '../components/Layout';
 
 class Index extends React.Component {
-  static async getInitialProps() {
+  static async getInitialProps({ req, res, query }) {
     let stories;
+    let page;
     try {
+      page = +query.page || 1;
       const response = await fetch(
-        'https://node-hnapi.herokuapp.com/news?page=1',
+        `https://node-hnapi.herokuapp.com/news?page=${page}`,
       );
 
       stories = await response.json();
@@ -17,10 +20,10 @@ class Index extends React.Component {
       console.log(error);
       stories = [];
     }
-    return { stories };
+    return { stories, page };
   }
   render() {
-    const { stories } = this.props;
+    const { stories, page } = this.props;
     if (stories.length === 0) {
       return <Error statusCode={503} />;
     }
@@ -30,6 +33,27 @@ class Index extends React.Component {
         description='A hacker News Clone Website built with Next.js'
       >
         <StoryList stories={stories} />
+        <footer>
+          {page > 1 && (
+            <Link href={`/?page=${page - 1}`}>
+              <a>Previous Page ({page - 1})</a>
+            </Link>
+          )}
+          <Link href={`/?page=${page + 1}`}>
+            <a>Next Page ({page + 1})</a>
+          </Link>
+        </footer>
+        <style jsx>{`
+          footer {
+            padding: 1em;
+          }
+          footer a {
+            font-weight: bold;
+            color: black;
+            text-decoration: none;
+            margin-right: 1em;
+          }
+        `}</style>
       </Layout>
     );
   }
